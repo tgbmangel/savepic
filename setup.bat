3ab29e58f3de412fb7aa0090dfca5a7234698ba2
Shr Zhu <tgbmangel@163.com>
2016-03-21 17:45:38 +0800

    Added files via upload
    提交初版文件

---
+++ setup.bat
@@ -0,0 +1,4 @@
+@echo off
+rd /s/q dist
+rd /s/q build
+python setup.py py2exe
---
+++ wa55Grab.py
@@ -0,0 +1,40 @@
+#coding:cp936
+import Tkinter as tk
+import tkMessageBox
+from PIL import ImageGrab
+import time,os
+
+def savepic():
+    if getpath():
+        addr_path=getpath()
+    else:
+        tkMessageBox.showinfo(title='without path',message="without path!pic will save to \"E:\\1\"")
+        var.set("E:\\1")
+        addr_path=getpath()
+
+    if not os.path.exists(addr_path):
+        os.makedirs(addr_path)
+    examtime=time.strftime("%Y%m%d%H%M%S",time.localtime())
+    addr=os.path.normcase(addr_path+"\\"+examtime+".jpeg")
+    im = ImageGrab.grab()
+    im.save(addr,'jpeg')
+    tkMessageBox.showinfo(title='done',message="pic save done!")
+
+def getpath():
+    filepath=var.get()
+    return filepath
+
+root =tk.Tk()
+root.title("GrabWA55")
+root.geometry('200x100')
+
+pathLable=tk.Label(root,text=u'´æÍ¼Â·¾¶:')
+pathLable.pack()
+
+var=tk.StringVar()
+e=tk.Entry(root,textvariable=var)
+var.set("E:\\1")
+e.pack()
+
+tk.Button(root, width=10,heigh=4,fg="blue",text="Save", command = savepic).pack()
+root.mainloop()
