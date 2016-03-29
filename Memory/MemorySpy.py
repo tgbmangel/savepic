904a72c013c598fac0a82b0eaf0d996f978f5ae7
Shr Zhu <tgbmangel@163.com>
2016-03-30 00:45:37 +0800

    Update MemorySpy.py
    在运行过程中程序w1中断导致的异常处理

--- Memory/MemorySpy.py
+++ Memory/MemorySpy.py
@@ -1,23 +1,25 @@
 import psutil,time
 import Tkinter as tk
+exename="w1.exe"
 def getpid():
     all_processes = list(psutil.process_iter())
     for p in all_processes:
         try:
-            if p.name()=="w1.exe":
+            if p.name()==exename:
                 w1=p
-                # print p.name(),p.pid,p.username()
-                # print p.get_cpu_percent(interval=0)
-                # print p.get_memory_info(),p.get_memory_percent()
-                # print p.get_cpu_times()
                 return w1
         except Exception as e:
             pass
 w1=getpid()
 def getMemery():
-    text2=str(float(w1.get_memory_info()[0]/1024.0/1024.0))+'M '+w1.name()
-    a.configure(text=text2)
-    root.after(1000,getMemery)
+    if not w1.name()==exename:
+        a.configure(text="not exist:"+str(exename))
+        w1=getpid()
+        root.after(2000,getMemery)
+    else:    
+        text2=str(float(w1.get_memory_info()[0]/1024.0/1024.0))+'M '+w1.name()
+        a.configure(text=text2)
+        root.after(1000,getMemery)
 
 root =tk.Tk()
 root.wm_attributes('-topmost',1)
