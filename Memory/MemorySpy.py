cef8eb786dc05f58e4cf05c4348ee71c090b06d5
Shr Zhu <tgbmangel@163.com>
2017-04-17 16:24:24 +0800

    Update MemorySpy.py
    更新代码规范

--- Memory/MemorySpy.py
+++ Memory/MemorySpy.py
@@ -1,38 +1,44 @@
+#coding:cp936
 import psutil,time
 import Tkinter as tk
-exename="w1.exe"
-def getpid():
+#memory
+EXE_PROCESS_NAME="w1.exe"
+def get_pid():
     all_processes = list(psutil.process_iter())
     for p in all_processes:
         try:
-            if p.name()==exename:
+            if p.name()==EXE_PROCESS_NAME:
                 w1=p
+                # print p.name(),p.pid,p.username()
+                # print p.get_cpu_percent(interval=0)
+                #print type(p.get_memory_info()),p.get_memory_percent()
+                # print p.get_cpu_times()
                 return w1
         except Exception as e:
             pass
 
-def getMemory():
+def get_memory():
     try:
-        w1=getpid()
-        if not w1.name()==exename:
-            a.configure(text="not exist:"+str(exename))
-            w1=getpid()
-            root.after(2000,getMemory)
-        else:    
-            text2=str(float(w1.get_memory_info()[0]/1024.0/1024.0))+'M '+w1.name()
+        w1=get_pid()
+        if not w1.name()==EXE_PROCESS_NAME:
+            a.configure(text="not exist:"+str(EXE_PROCESS_NAME))
+            w1=get_pid()
+            root.after(2000,get_memory)
+        else:
+            text2=str(w1.memory_info()[0]/1024.0/1024.0)+'M '+w1.name()
             a.configure(text=text2)
-            root.after(1000,getMemory)
+            root.after(1000,get_memory)
     except Exception:
-        a.configure(text="not exist:"+str(exename))
+        a.configure(text="not exist:"+str(EXE_PROCESS_NAME))
         #w1=getpid()
-        root.after(2000,getMemory)
-
-root =tk.Tk()
-root.wm_attributes('-topmost',1)
-root.resizable(False, False)
-root.title("Memory")
-root.geometry('300x30')
-a=tk.Label(root)
-a.pack()
-getMemory()
-root.mainloop()
+        root.after(2000,get_memory)
+if __name__=="__main__":
+    root =tk.Tk()
+    root.wm_attributes('-topmost',1)
+    root.resizable(False, False)
+    root.title(u"w1进程内存")
+    root.geometry('230x30')
+    a=tk.Label(root)
+    a.pack()
+    get_memory()
+    root.mainloop()
